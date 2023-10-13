bed4199d7934d369eb8761d74ac1f83a01a6e167
Roman Kabaev <r.kabaev@yadro.com>
2023-10-13 10:06:02 +0300

    Added licensing

--- plugin_example/__init__.py
+++ plugin_example/__init__.py
@@ -35,7 +35,7 @@ class ExamplePluginConfig(TestyPluginConfig):
     name = 'plugin_example'
     verbose_name = 'Plugin example'
     description = 'It is very simple plugin example'
-    version = '0.1'
+    version = '0.1.0'
     plugin_base_url = 'plugin-example'
     index_reverse_name = 'upload-file'
 
